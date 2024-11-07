3743027d176987e2507e944b90a18c8a87fb8fcd
Aditia <aditpraz222@gmail.com>
2024-11-08 00:30:27 +0700

    Update router and component imports

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,104 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <div id="app">
+        <Header
+            :currentRole="currentRole"
+            @update-role="updateRole"
+            @toggle-sidebar="toggleSidebar"
+            :isSidebarVisible="isSidebarVisible"
+        />
+        <div class="app-content">
+            <Sidebar
+                :currentRole="currentRole"
+                :isSidebarVisible="isSidebarVisible"
+                @showComponent="navigateTo"
+            />
+            <div class="main-content" :class="{ expanded: isSidebarVisible }">
+                <router-view
+                    :key="$route.fullPath"
+                    :currentComponent="$route.params.component"
+                />
+            </div>
+        </div>
+    </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import Header from './components/dashboard/Header.vue';
+import Sidebar from './components/dashboard/Sidebar.vue';
+import { EventBus } from './utils/EventBus'
+
+export default {
+    components: {
+        Header,
+        Sidebar,
+    },
+    data() {
+        return {
+            currentRole: this.$route.name || 'admin',
+            isSidebarVisible: true,
+            searchTerm: ''
+        }
+    },
+    watch: {
+        '$route.name'(newRole) {
+            this.currentRole = newRole
+        }
+    },
+    computed: {
+        currentView() {
+            return this.currentRole === 'admin' ? AdminView : UserView
+        }
+    },
+    methods: {
+        updateRole(role) {
+            this.currentRole = role
+            this.navigateTo('items')
+        },
+        navigateTo(component) {
+            this.$router.push({ name: this.currentRole, params: { component } })
+        },
+        toggleSidebar() {
+            this.isSidebarVisible = !this.isSidebarVisible
+        },
+        handleSearch(newQuery) {
+            console.log('Search term:', newQuery)
+            if (this.currentRole === 'admin') {
+                console.log('Search in admin items')
+            } else if (this.currentRole === 'user') {
+                console.log('Search in user items')
+            }
+        }
+    },
+    mounted() {
+        EventBus.on('search', this.handleSearch)
+    },
+    beforeUnmount() {
+        EventBus.off('search', this.handleSearch)
+    }
+}
+</script>
+
+<style scoped>
+.app-content {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    display: flex;
+    font: 1em sans-serif;
+    height: 100vh;
+    margin-top: 60px;
 }
 
-nav {
-  padding: 30px;
+.main-content {
+    flex-grow: 1;
+    transition: margin-left 0.3s ease;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.main-content.expanded {
+    margin-left: 200px;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+@media (max-width: 768px) {
+    .main-content {
+        margin-left: 0;
+    }
 }
 </style>
