7210d9b58cf87fc289fececfc16d34d16ae0d22a
Aditia <aditpraz222@gmail.com>
2024-11-07 23:15:32 +0700

    Add router configuration to improve route handling

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import "./assets/styles/global.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import router from "./router";
+import router from "./router"; // Only one router import
 import { createPinia } from "pinia";
-import axios from "axios";
+import axios from "axios"; // This import can stay if you're using axios elsewhere in the app
 
-createApp(App).use(createPinia()).use(router).mount("#app");
+createApp(App).use(router).use(createPinia()).mount("#app");
