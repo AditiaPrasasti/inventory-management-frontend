af252ef08ba0a5988bc76e10d6a6230911a0bd95
Aditia <aditpraz222@gmail.com>
2024-11-21 16:05:04 +0700

    feat (api): add new endpoint and fix routing

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <template>
   <div id="app">
     <Header
+      v-if="showHeader"
       :currentRole="currentRole"
       @update-role="updateRole"
       @toggle-sidebar="toggleSidebar"
       :isSidebarVisible="isSidebarVisible"
     />
-    <div class="app-content">
+
+    <div class="app-content" :class="{ noHeader: !showHeader }">
       <Sidebar
+        v-if="showSidebar"
         :currentRole="currentRole"
         :isSidebarVisible="isSidebarVisible"
         @showComponent="navigateTo"
       />
-      <div class="main-content" :class="{ expanded: isSidebarVisible }">
+
+      <div
+        class="main-content"
+        :class="{ expanded: isSidebarVisible && showSidebar }"
+      >
         <router-view
           :key="$route.fullPath"
           :currentComponent="$route.params.component"
@@ -32,6 +39,7 @@ export default {
     Header,
     Sidebar,
   },
+
   data() {
     return {
       currentRole: this.$route.name || "admin",
@@ -39,29 +47,44 @@ export default {
       searchTerm: "",
     };
   },
+
+  computed: {
+    showHeader() {
+      return !this.$route.meta.hideHeader;
+    },
+    showSidebar() {
+      return !this.$route.meta.hideSidebar;
+    },
+  },
+
   watch: {
     "$route.name"(newRole) {
       this.currentRole = newRole;
     },
   },
-  computed: {
-    currentView() {
-      return this.currentRole === "admin" ? AdminView : UserView;
-    },
-  },
+
   methods: {
     updateRole(role) {
       this.currentRole = role;
-      this.navigateTo("items");
     },
+
     navigateTo(component) {
-      this.$router.push({ name: this.currentRole, params: { component } });
+      if (this.currentRole === "ADMIN") {
+        this.$router.push({ name: "admin", params: { component } });
+      } else if (this.currentRole === "USER") {
+        this.$router.push({ name: "user" });
+      } else {
+        this.$router.push({ name: "login" });
+      }
     },
+
     toggleSidebar() {
       this.isSidebarVisible = !this.isSidebarVisible;
     },
+
     handleSearch(newQuery) {
       console.log("Search term:", newQuery);
+
       if (this.currentRole === "admin") {
         console.log("Search in admin items");
       } else if (this.currentRole === "user") {
@@ -69,9 +92,11 @@ export default {
       }
     },
   },
+
   mounted() {
     EventBus.on("search", this.handleSearch);
   },
+
   beforeUnmount() {
     EventBus.off("search", this.handleSearch);
   },
--- src/router/index.js
+++ src/router/index.js
@@ -1,37 +1,43 @@
 import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 import AdminView from "../views/AdminView.vue";
 import UserView from "../views/UserView.vue";
-import LoginView from "@/views/LoginView.vue";
+import Login from "@/components/auth/Login.vue";
+import Register from "@/components/auth/Register.vue";
+import { useAuthStore } from "@/store/authStore";
 
 const routes = [
   {
-    path: "/admin/:component",
+    path: "/",
+    name: "home",
+    component: HomeView,
+    meta: { hideHeader: true, hideSidebar: true },
+    children: [
+      {
+        path: "login",
+        name: "login",
+        component: Login,
+      },
+      {
+        path: "register",
+        name: "register",
+        component: Register,
+      },
+    ],
+  },
+  {
+    path: "/admin/:component?",
     name: "admin",
     component: AdminView,
     props: true,
-    meta: {
-      requireAuth: true,
-      role: "admin",
-    },
+    meta: { requiresAuth: true, role: "ADMIN" },
   },
   {
-    path: "/user/:component",
+    path: "/user/:component?",
     name: "user",
     component: UserView,
     props: true,
-    meta: {
-      requireAuth: true,
-      role: "user",
-    },
-  },
-  {
-    path: "/login",
-    name: "login",
-    component: LoginView,
-  },
-  {
-    path: "/",
-    redirect: { name: "admin", params: { component: "items" } },
+    meta: { requiresAuth: true, role: "USER" },
   },
 ];
 
@@ -41,18 +47,20 @@ const router = createRouter({
 });
 
 router.beforeEach((to, from, next) => {
-  const isAunthenticated = localStorage.getItem("auth");
-  const userRole = localStorage.getItem("role");
+  const authStore = useAuthStore();
+  const isAuthenticated = !!authStore.token;
+  const userRole = authStore.role;
 
-  if (to.meta.requireAuth && !isAunthenticated) {
-    next({ name: "login" });
-  } else if (
-    to.meta.requireAuth &&
-    isAunthenticated &&
-    to.meta.role !== userRole
-  ) {
-    alert("You are not authorized to access this page");
-    next(false);
+  if (to.meta.requiresAuth) {
+    if (isAuthenticated) {
+      if (userRole === to.meta.role || to.meta.role === undefined) {
+        next();
+      } else {
+        next({ name: "home" });
+      }
+    } else {
+      next({ name: "home" });
+    }
   } else {
     next();
   }
