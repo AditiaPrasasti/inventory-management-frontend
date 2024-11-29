06c45a882c7dfe4b059fa98f10db524e4a0520ac
Aditia <aditpraz222@gmail.com>
2024-11-29 13:22:24 +0700

    fix(axios): update baseURL

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useAuthStore } from "@/store/authStore";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://inventory-management-three-rho.vercel.app/",
   headers: {
     "Content-Type": "application/json",
   },
